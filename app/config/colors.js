389e03323437c616a0088f86f435d3a7d4ffa010
Beau <halebeau3@gmail.com>
2022-06-03 17:59:19 -0500

    Added: Card, images, AppText, ListItem, colors

--- app/config/colors.js
+++ app/config/colors.js
@@ -1,6 +1,5 @@
 export default {
-   primary: "#fc5c65",
-   secondary: "#4ecdc4",
-   black: "#000",
-   white: '#fff'
+  primary: "#fc5c65",
+  secondary: "#4ecdc4",
+  black: "#000",
 };
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,51 +1,47 @@
-import React from 'react';
-import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
-import AppButton from '../components/AppButton';
+import React from "react";
+import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
 
 function WelcomeScreen(props) {
-   return (
-      <ImageBackground
-      blurRadius={10}
+  return (
+    <ImageBackground
       style={styles.background}
-         source={require('../assets/background.jpg')}
-      >
-         <View style={styles.logoContainer}>
-         <Image style={styles.logo} source={require('../assets/logo-red.png')} />
-         <Text style={styles.tagline}>Sell What You Don't Need</Text>
-         </View>
-         <View style={styles.buttonsContainer}>
-         <AppButton title="Login" />
-         <AppButton title="Register" color="secondary" />
-         </View>
-      </ImageBackground>
-   );
+      source={require("../assets/background.jpg")}
+    >
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+        <Text>Sell What You Don't Need</Text>
+      </View>
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
+  );
 }
 
 const styles = StyleSheet.create({
-   background: {
-      flex: 1,
-      justifyContent: "flex-end",
-      alignItems: 'center',
-   },
-   buttonsContainer: {
-      padding: 20,
-      width: '100%',
-   },
-   logo: {
-      width: 100,
-      height: 100,
-   },
-   logoContainer: {
-      position: 'absolute',
-      top: 70,
-      alignItems: 'center'
-   },
-   tagline: {
-      fontSize: 25,
-      fontWeight: "600",
-      paddingVertical: 20,
-
-   }
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#fc5c65",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4ecdc4",
+  },
 });
 
 export default WelcomeScreen;
