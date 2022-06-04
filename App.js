f75b8c72fa601f4b7ae8967d79ebfb45067af9f6
Beau <halebeau3@gmail.com>
2022-06-04 16:50:28 -0500

    Added: MessagesScreen

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { View } from "react-native";
-import Card from "./app/components/Card";
 
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
+import MessagesScreen from "./app/screens/MessagesScreen"
+
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <MessagesScreen />
 }
